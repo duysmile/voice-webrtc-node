baf6e70508d6beebd3cdc215a965f75c3ace0964
Duy Nguyen <duy210697@gmail.com>
2022-02-04 14:46:05 +0700

    add video call

--- public/script.js
+++ public/script.js
@@ -1,15 +1,48 @@
+const socket = io('/');
+
 let myVideoStream;
 
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+const peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030',
+});
+
 navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+    peer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        });
+    });
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
+    });
 });
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    });
+};
+
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const uuid = require('uuid').v4;
 const express = require('express');
+const socketio = require('socket.io');
+const { ExpressPeerServer } = require('peer');
 
 const app = express();
 const server = require('http').Server(app);
+const io = socketio(server, {
+    allowEIO3: true,
+});
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
 
+app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.get('/', (_, res) => {
@@ -16,6 +25,14 @@ app.get('/:room', (req, res) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server started at port 3000");
+io.on('connection', socket => {
+    console.log('connected');
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    });
+});
+
+server.listen(3030, () => {
+    console.log("Server started at: http://localhost:3030");
 });
